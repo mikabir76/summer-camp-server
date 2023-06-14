b09024d8fbc001682d2fc3db356d74a1cbd4046e
mikabir76 <kabirmd0987@gmail.com>
2023-06-14 22:38:55 +0600

    my class deleted successfully

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
@@ -38,9 +38,25 @@ app.get('/classes', async(req, res)=>{
     res.send(result)
 })
 // My Class Api
+
+app.get('/myclass', async(req, res)=>{
+    const email = req.query.email;
+    if(!email){
+        res.send([])
+    }
+    const query = {email: email};
+    const result = await myClassCollection.find(query).toArray();
+    res.send(result)
+})
 app.post('/myclass', async(req, res)=>{
     const classes = req.body;
     const result = await myClassCollection.insertOne(classes);
+    res.send(result)
+});
+app.delete('/myclass/:id', async(req, res)=>{
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)};
+    const result = await myClassCollection.deleteOne(query)
     res.send(result)
 })
 
