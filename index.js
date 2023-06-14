daa23b945b97783f1c461421cfabf8f5e04614b4
mikabir76 <kabirmd0987@gmail.com>
2023-06-14 16:24:55 +0600

    mongodb atlas and .env secure

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const app = express()
+const app = express();
+require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
@@ -13,7 +14,7 @@ app.use(express.json())
 
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.pnta7vo.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pnta7vo.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
