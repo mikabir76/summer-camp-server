e71667080e8f429a38e3dd6fbed4f479dfb0ce4b
mikabir76 <kabirmd0987@gmail.com>
2023-06-15 23:30:27 +0600

    stripe card install

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
+const stripe = require("stripe")(process.env.SECRET_KEY_API)
 const port = process.env.PORT || 5000;
 
 // middleWare
@@ -102,7 +103,7 @@ app.get('/users/admin/:email', verifyJWT, async(req, res)=>{
 
 app.patch('/users/admin/:id',async(req, res)=>{
   const id = req.params.id;
-  console.log(id)
+  // console.log(id)
   const filter = {_id: new ObjectId(id)};
   const updateUser = {
     $set:{
@@ -126,7 +127,7 @@ app.get('/myclass', verifyJWT, async(req, res)=>{
         res.send([])
     };
     const decodedEmail = req.decoded.email;
-    console.log(req.decoded)
+    // console.log(req.decoded)
     if(email !== decodedEmail){
     return  res.status(401).send({error: true, message: 'Forbidden Access'})
     
